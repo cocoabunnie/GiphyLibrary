8a10ccd9f2bc8ca99ba98dfe46e8f488d66fb77d
duvivierbri <duvivierbri@gmail.com>
2021-06-12 19:17:13 -0400

    added the gifcard to render in the results component

--- mygiffyapp/src/Components/GifCard.js
+++ mygiffyapp/src/Components/GifCard.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 function GifCard(props){
     return (
         <div>
+            <p>Test Image</p>
             <img src={props.imageURL} />
         </div>
     )
--- mygiffyapp/src/Components/Results.js
+++ mygiffyapp/src/Components/Results.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react"
 import ReactDOM from 'react-dom';
 import SearchFieldComponent from "./SearchFieldComponent";
+import GifCard from "./GifCard";
 
 class Results extends Component{
-    
-
     render(){
         return(
             <div>
                 <SearchFieldComponent />
+                <GifCard imageURL=""/>
             </div>
         )
     }
