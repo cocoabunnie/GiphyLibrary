0dc72bc19983c7acbe5dfa56b2580aa56e92e0ca
duvivierbri <duvivierbri@gmail.com>
2021-06-12 19:48:37 -0400

    We now get the trending information from giphy with showTrending() function

--- mygiffyapp/src/Components/Results.js
+++ mygiffyapp/src/Components/Results.js
@@ -14,14 +14,23 @@ class Results extends Component{
         }
     }
 
-    getInformation = () => {
-        
+    showTrending = () => {
+        let myAPI = "rGQfGquBOeYnM9jwCFYyoZBkB5lRWvfy";
+        let trendingURL = "http://api.giphy.com/v1/gifs/trending?api_key=" + myAPI;
+        fetch(trendingURL)
+        .then(response => response.json())
+        .then(trendingGifs => {
+            this.setState({
+                gifs: trendingGifs
+            })
+            console.log(this.state.gifs);
+        })
     }
 
     render(){
         return(
             <div>
-                <SearchFieldComponent />
+                <SearchFieldComponent searchFunction={this.showTrending}/>
                 <GifCard imageURL=""/>
             </div>
         )
